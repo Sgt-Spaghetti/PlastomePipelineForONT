a22d02831e83c2c34a09bbf96ea8f8f5c30b20c0
Sgt-Spaghetti <43071419+Sgt-Spaghetti@users.noreply.github.com>
2025-05-13 17:26:06 +0100

    Delete Pipeline directory

--- Pipeline/PlastomePipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -ne 4 ]; then
-    echo "Usage: bash PlastomePipeline.sh *Input Directory Name* *AT% bottom threshold* *AT% top threshold* *Minimum Read Length (bp)*\n eg. bash PlastomePipeline.sh RawData 60 100 10000"
-else
-    bash ATCleanerLauncher.sh $1 $2 $3 $4
-    bash pipeline_alignment.sh
-fi
--- Pipeline/pipeline_alignment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-minimap2 -ax map-ont /home/leo/Documents/Work/Algae2025/Genomes/csb516_cpv4-simp.fasta /home/leo/Documents/Work/Algae2025/ATCleanerOutput/* > /home/leo/Documents/Work/Algae2025/alignment.sam
-
-samtools view -@ n -Sb -o alignment.bam alignment.sam
-
-samtools sort -O bam -o sorted_alignment.bam alignment.bam
-
-samtools index sorted_alignment.bam
