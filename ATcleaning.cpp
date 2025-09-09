65142025d118456ae25ea82eefc67bdf6a3071c1
Sgt-Spaghetti <43071419+Sgt-Spaghetti@users.noreply.github.com>
2025-09-09 22:49:20 +0100

    Memory Safe

--- ATcleaning.cpp
+++ ATcleaning.cpp
@@ -5,6 +5,47 @@
 
 using namespace std;
 
+class Entry {
+
+	private:
+		string * pread_name;
+		string * pread_sequence;
+		string * pread_adapter;
+		string * pread_quality;
+		
+	public:
+		~Entry(){
+			if(pread_name!=NULL)  delete pread_name;
+			if(pread_sequence!=NULL)  delete pread_sequence;
+			if(pread_adapter!=NULL) delete pread_adapter;
+			if(pread_quality!=NULL) delete pread_quality;
+		}
+		void set_name(string * n){
+			pread_name = n;
+		}
+		void set_sequence(string * n){
+			pread_sequence = n;
+		}
+		void set_adapter(string * n){
+			pread_adapter = n;
+		}
+		void set_quality(string * n){
+			pread_quality = n;
+		}
+		string * get_name(){
+			return pread_name;
+		}
+		string * get_sequence(){
+			return pread_sequence;
+		}
+		string * get_adapter(){
+			return pread_adapter;
+		}
+		string * get_quality(){
+			return pread_quality;
+		}
+};
+
 bool CheckReadParameters(const string &Read, const int &ATminimum, const int &ATmaximum, const int &MinimumLength){ // returns true to discard a read, or false to keep it
 	const size_t ReadLength{Read.length()};
 	if (ReadLength < MinimumLength){
@@ -55,7 +96,7 @@ int main(int argc, char* argv[]) {
 	
 	const int LinesPerEntry{4};
 	int PointInEntry{0};
-	string Entry[4]{"","","",""};
+
 	const int LocationOfSequence{1};
 	bool DiscardEntry{false};
 
@@ -63,27 +104,40 @@ int main(int argc, char* argv[]) {
 
 	ifstream InputFile(InputFilePath);
 
-	string LineInFile;
+	string *LineInFile = new string();
 
-	while (getline(InputFile, LineInFile)){
+	Entry * pnew_entry = new Entry();
+
+	while (getline(InputFile, *LineInFile)){
 		if (PointInEntry<LinesPerEntry-1){
-			Entry[PointInEntry] = LineInFile;
+			if (PointInEntry == 0){
+				 pnew_entry -> set_name(LineInFile);
+			} else if (PointInEntry == 1){
+				pnew_entry -> set_sequence(LineInFile);
+			} else if (PointInEntry == 2){
+				pnew_entry -> set_adapter(LineInFile);
+			}
+
 			if (PointInEntry == LocationOfSequence){
-				DiscardEntry = CheckReadParameters(Entry[PointInEntry], ATmin, ATmax, LengthMin);
+				DiscardEntry = CheckReadParameters(*(pnew_entry -> get_sequence()), ATmin, ATmax, LengthMin);
 			}
 			++PointInEntry;
 		}
 		else { // We are in the last section of the entry, prepare reset
-			Entry[PointInEntry] = LineInFile; // We are at the quality score!
+			pnew_entry -> set_quality(LineInFile); // We are at the quality score!
 			if (DiscardEntry == false){ // It has passed the AT etc checks
-				if (CheckReadQuality(Entry[PointInEntry], QualityMin) == false){ // Can it pass the quality score check?
-					for (int i = 0; i < LinesPerEntry; i++){
-						EntriesToKeep.push_back(Entry[i]);
-					}
+				if (CheckReadQuality(*(pnew_entry -> get_quality()), QualityMin) == false){ // Can it pass the quality score check?
+					EntriesToKeep.push_back(*(pnew_entry -> get_name()));
+					EntriesToKeep.push_back(*(pnew_entry -> get_sequence()));
+					EntriesToKeep.push_back(*(pnew_entry -> get_adapter()));
+					EntriesToKeep.push_back(*(pnew_entry -> get_quality()));
 				}
 			}
 			PointInEntry = 0;
+			delete pnew_entry;
+			Entry * pnew_entry = new Entry();
 		}
+	LineInFile = new string();
 	}
 
 	InputFile.close();
